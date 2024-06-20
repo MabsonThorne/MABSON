61aafb5b310632a8da2d9268b791fd0719b4ae40
MabsonThorne <mabsonthornne@gmail.com>
2024-06-20 23:52:18 +0800

    Update product.js

--- backend/models/product.js
+++ backend/models/product.js
@@ -1,10 +1,24 @@
 const db = require('../config/db');
 
 const Product = {
+  create: (productData, callback) => {
+    const { name, price, quantity, imageUrl, userId, description } = productData;
+    db.query('INSERT INTO products (name, price, quantity, imageUrl, userId, description) VALUES (?, ?, ?, ?, ?, ?)', [name, price, quantity, imageUrl, userId, description], (err, results) => {
+      if (err) return callback(err);
+      callback(null, { id: results.insertId, ...productData });
+    });
+  },
+
   getAll: (callback) => {
     db.query('SELECT * FROM products', callback);
   },
-  // 添加其他商品相关数据库操作
+
+  findById: (id, callback) => {
+    db.query('SELECT * FROM products WHERE id = ?', [id], (err, results) => {
+      if (err) return callback(err);
+      callback(null, results[0]);
+    });
+  },
 };
 
 module.exports = Product;
