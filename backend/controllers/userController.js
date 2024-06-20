bb628237715e41e5614bedb0dd4bf2a7512d9857
MabsonThorne <mabsonthornne@gmail.com>
2024-06-20 23:35:19 +0800

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,34 @@
 const User = require('../models/user');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+exports.register = (req, res) => {
+  const { username, password, email } = req.body;
+  const hashedPassword = bcrypt.hashSync(password, 10);
+
+  User.create({ username, password: hashedPassword, email }, (err, user) => {
+    if (err) return res.status(500).send(err);
+    res.status(201).send('User registered successfully');
+  });
+};
+
+exports.login = (req, res) => {
+  const { email, password } = req.body;
+
+  User.findByEmail(email, (err, user) => {
+    if (err) return res.status(500).send(err);
+    if (!user) return res.status(404).send('User not found');
+
+    const isValidPassword = bcrypt.compareSync(password, user.password);
+    if (!isValidPassword) return res.status(401).send('Invalid password');
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: '1h',
+    });
+
+    res.json({ token });
+  });
+};
 
 exports.getAllUsers = (req, res) => {
   User.getAll((err, users) => {
