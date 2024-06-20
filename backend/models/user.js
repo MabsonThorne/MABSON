7a9b5ea594ac2e7a4659b501287ffad35530be5e
MabsonThorne <mabsonthornne@gmail.com>
2024-06-20 23:52:08 +0800

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,19 +2,31 @@ const db = require('../config/db');
 
 const User = {
   create: (userData, callback) => {
-    const { username, password, email } = userData;
-    db.query('INSERT INTO users (username, password, email) VALUES (?, ?, ?)', [username, password, email], callback);
+    const { username, password, email, role } = userData;
+    db.query('INSERT INTO user_register (username, password, email, role) VALUES (?, ?, ?, ?)', [username, password, email, role], (err, results) => {
+      if (err) return callback(err);
+      const userId = results.insertId;
+      db.query('INSERT INTO user_info (id, username, email, role) VALUES (?, ?, ?, ?)', [userId, username, email, role], (err) => {
+        if (err) return callback(err);
+        callback(null, { id: userId, username, email, role });
+      });
+    });
   },
 
   findByEmail: (email, callback) => {
-    db.query('SELECT * FROM users WHERE email = ?', [email], (err, results) => {
+    db.query('SELECT * FROM user_register WHERE email = ?', [email], (err, results) => {
       if (err) return callback(err);
       callback(null, results[0]);
     });
   },
 
+  updateInfo: (id, data, callback) => {
+    const { avatar, bio } = data;
+    db.query('UPDATE user_info SET avatar = ?, bio = ? WHERE id = ?', [avatar, bio, id], callback);
+  },
+
   getAll: (callback) => {
-    db.query('SELECT * FROM users', callback);
+    db.query('SELECT * FROM user_info', callback);
   },
 };
 
