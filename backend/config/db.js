c03a7a165633902e657bc2747f6cdae167db7c55
MabsonThorne <mabsonthornne@gmail.com>
2024-06-20 23:58:52 +0800

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: 'user_register_db'
 });
 
 connection.connect((err) => {
