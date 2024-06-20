0832919b963edca94111ba28dec8b599af7aeefc
MabsonThorne <mabsonthornne@gmail.com>
2024-06-20 23:35:59 +0800

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const auth = require('../middleware/auth');
 
-router.get('/users', userController.getAllUsers);
+router.post('/register', userController.register);
+router.post('/login', userController.login);
+router.get('/users', auth, userController.getAllUsers);
 
 module.exports = router;
