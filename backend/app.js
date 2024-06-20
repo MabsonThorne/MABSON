a9bff900aa5a666f825c92109bc8506312f442ba
MabsonThorne <mabsonthornne@gmail.com>
2024-06-20 23:24:36 +0800

    Create app.js

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const userRoutes = require('./routes/userRoutes');
+const productRoutes = require('./routes/productRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+const translationRoutes = require('./routes/translationRoutes');
+const cors = require('./middleware/cors');
+const errorHandler = require('./middleware/errorHandler');
+require('dotenv').config();
+
+app.use(express.json());
+app.use(cors);
+
+app.use('/api', userRoutes);
+app.use('/api', productRoutes);
+app.use('/api', orderRoutes);
+app.use('/api', translationRoutes);
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://106.52.158.123:${PORT}`);
+});
