594ece2e47d6fc13250eb2c8fd2e74e682154712
MabsonThorne <mabsonthornne@gmail.com>
2024-06-20 23:35:42 +0800

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,10 +1,21 @@
 const db = require('../config/db');
 
 const User = {
+  create: (userData, callback) => {
+    const { username, password, email } = userData;
+    db.query('INSERT INTO users (username, password, email) VALUES (?, ?, ?)', [username, password, email], callback);
+  },
+
+  findByEmail: (email, callback) => {
+    db.query('SELECT * FROM users WHERE email = ?', [email], (err, results) => {
+      if (err) return callback(err);
+      callback(null, results[0]);
+    });
+  },
+
   getAll: (callback) => {
     db.query('SELECT * FROM users', callback);
   },
-  // 添加其他用户相关数据库操作
 };
 
 module.exports = User;
