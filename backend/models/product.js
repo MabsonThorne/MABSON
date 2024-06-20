abaa7d7d54833e9bbf077a5ad92d51d61500be23
MabsonThorne <mabsonthornne@gmail.com>
2024-06-20 23:59:21 +0800

    Update product.js

--- backend/models/product.js
+++ backend/models/product.js
@@ -3,18 +3,18 @@ const db = require('../config/db');
 const Product = {
   create: (productData, callback) => {
     const { name, price, quantity, imageUrl, userId, description } = productData;
-    db.query('INSERT INTO products (name, price, quantity, imageUrl, userId, description) VALUES (?, ?, ?, ?, ?, ?)', [name, price, quantity, imageUrl, userId, description], (err, results) => {
+    db.query('INSERT INTO product_db.products (name, price, quantity, imageUrl, userId, description) VALUES (?, ?, ?, ?, ?, ?)', [name, price, quantity, imageUrl, userId, description], (err, results) => {
       if (err) return callback(err);
       callback(null, { id: results.insertId, ...productData });
     });
   },
 
   getAll: (callback) => {
-    db.query('SELECT * FROM products', callback);
+    db.query('SELECT * FROM product_db.products', callback);
   },
 
   findById: (id, callback) => {
-    db.query('SELECT * FROM products WHERE id = ?', [id], (err, results) => {
+    db.query('SELECT * FROM product_db.products WHERE id = ?', [id], (err, results) => {
       if (err) return callback(err);
       callback(null, results[0]);
     });
