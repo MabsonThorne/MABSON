56d5dd8bdc269eb560cb6f9c31467e028cff1164
MabsonThorne <mabsonthornne@gmail.com>
2024-06-22 14:39:43 +0800

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,10 +3,10 @@ const db = require('../config/db');
 const User = {
   create: (userData, callback) => {
     const { username, password, email, role } = userData;
-    db.query('INSERT INTO user_register (username, password, email, role) VALUES (?, ?, ?, ?)', [username, password, email, role], (err, results) => {
+    db.query('INSERT INTO users (username, password, email, role) VALUES (?, ?, ?, ?)', [username, password, email, role], (err, results) => {
       if (err) return callback(err);
       const userId = results.insertId;
-      db.query('INSERT INTO user_info_db.user_info (id, username, email, role) VALUES (?, ?, ?, ?)', [userId, username, email, role], (err) => {
+      db.query('INSERT INTO user_profiles (user_id, username, email, role) VALUES (?, ?, ?, ?)', [userId, username, email, role], (err) => {
         if (err) return callback(err);
         callback(null, { id: userId, username, email, role });
       });
@@ -14,14 +14,14 @@ const User = {
   },
 
   findByEmail: (email, callback) => {
-    db.query('SELECT * FROM user_register WHERE email = ?', [email], (err, results) => {
+    db.query('SELECT * FROM users WHERE email = ?', [email], (err, results) => {
       if (err) return callback(err);
       callback(null, results[0]);
     });
   },
 
   findById: (id, callback) => {
-    db.query('SELECT * FROM user_info_db.user_info WHERE id = ?', [id], (err, results) => {
+    db.query('SELECT * FROM user_profiles WHERE user_id = ?', [id], (err, results) => {
       if (err) return callback(err);
       callback(null, results[0]);
     });
@@ -29,11 +29,11 @@ const User = {
 
   updateInfo: (id, data, callback) => {
     const { avatar, bio } = data;
-    db.query('UPDATE user_info_db.user_info SET avatar = ?, bio = ? WHERE id = ?', [avatar, bio, id], callback);
+    db.query('UPDATE user_profiles SET avatar = ?, bio = ? WHERE user_id = ?', [avatar, bio, id], callback);
   },
 
   getAll: (callback) => {
-    db.query('SELECT * FROM user_info_db.user_info', callback);
+    db.query('SELECT * FROM user_profiles', callback);
   },
 };
 
