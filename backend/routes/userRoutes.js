a34fbe0581ce1a0c86597c39d7d5b7a25fb46baf
MabsonThorne <mabsonthornne@gmail.com>
2024-06-22 14:45:35 +0800

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,8 +5,9 @@ const auth = require('../middleware/auth');
 
 router.post('/register', userController.register);
 router.post('/login', userController.login);
+router.post('/check-email', userController.checkEmail); // 新增的检查邮箱路由
 router.put('/update', auth, userController.updateUserInfo);
 router.get('/users', auth, userController.getAllUsers);
-router.get('/profile', auth, userController.getUserProfile); // 新增获取用户资料的路由
+router.get('/profile', auth, userController.getUserProfile); // 获取用户资料路由
 
 module.exports = router;
