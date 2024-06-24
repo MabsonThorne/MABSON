53dee08369c3d8d2a78f98e68095e2963cb68898
MabsonThorne <mabsonthornne@gmail.com>
2024-06-24 17:12:07 +0800

    Update userProfile.js

--- backend/models/userProfile.js
+++ backend/models/userProfile.js
@@ -2,18 +2,18 @@ const db = require('../config/db');
 
 const UserProfile = {
   create: (data, callback) => {
-    const sql = 'INSERT INTO user_profiles (user_id, username, email, role, password) VALUES (?, ?, ?, ?, ?)';
-    db.query(sql, [data.user_id, data.username, data.email, data.role, data.password], callback);
+    const sql = 'INSERT INTO user_profiles (id, username, email, role) VALUES (?, ?, ?, ?)';
+    db.query(sql, [data.id, data.username, data.email, data.role], callback);
   },
   findByUserId: (user_id, callback) => {
-    const sql = 'SELECT * FROM user_profiles WHERE user_id = ?';
+    const sql = 'SELECT * FROM user_profiles WHERE id = ?';
     db.query(sql, [user_id], (err, results) => {
       if (err) return callback(err);
       callback(null, results[0]);
     });
   },
   update: (user_id, data, callback) => {
-    const sql = 'UPDATE user_profiles SET bio = ?, gender = ?, avatar_file = ? WHERE user_id = ?';
+    const sql = 'UPDATE user_profiles SET bio = ?, gender = ?, avatar_file = ? WHERE id = ?';
     db.query(sql, [data.bio, data.gender, data.avatar_file, user_id], callback);
   }
 };
