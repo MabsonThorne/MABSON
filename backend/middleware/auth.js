6a8f4db9cd359b79585ccae22edaac5986174ad5
MabsonThorne <mabsonthornne@gmail.com>
2024-06-29 00:42:19 +0800

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,29 +1,13 @@
 const jwt = require('jsonwebtoken');
-
-module.exports = (req, res, next) => {
-  const authHeader = req.header('Authorization');
-  if (!authHeader) {
-    console.error('Access denied. No token provided.');
-    return res.status(401).send('Access denied. No token provided.');
-  }
-
-  if (!authHeader.startsWith('Bearer ')) {
-    console.error('Access denied. Malformed token.');
-    return res.status(401).send('Access denied. Malformed token.');
-  }
-
-  const token = authHeader.replace('Bearer ', '');
-  if (!token) {
-    console.error('Access denied. No token provided.');
-    return res.status(401).send('Access denied. No token provided.');
-  }
-
+const auth = (req, res, next) => {
+  const token = req.cookies.authToken;
+  if (!token) return res.status(401).send('Access denied. No token provided.');
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
   } catch (ex) {
-    console.error('Invalid token.', ex);
     res.status(400).send('Invalid token.');
   }
 };
+module.exports = auth;
