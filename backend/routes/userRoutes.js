e68b4750b6d90d14fcbe3cfd4c52e96f7e151183
MabsonThorne <mabsonthornne@gmail.com>
2024-07-02 14:59:00 +0800

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,4 +1,4 @@
-// userRoutes.js
+// routes/userRoutes.js
 
 const express = require('express');
 const router = express.Router();
@@ -10,15 +10,16 @@ router.post('/register', userController.register);
 router.post('/login', userController.login);
 router.post('/check-email', userController.checkEmail);
 router.get('/users', auth, userController.getAllUsers);
-router.get('/profile', auth, userController.getUserProfile); // 获取当前用户的资料
-router.get('/profile/:id', auth, userController.getUserProfileById); // 获取特定用户的资料
-router.put('/user_profiles/:id', auth, upload.single('avatar_file'), userController.updateUserProfile); // 更新用户资料
-router.get('/public_profile/:id', userController.getPublicUserProfile); // 公开路由
+router.get('/profile', auth, userController.getUserProfile); 
+router.get('/profile/:id', auth, userController.getUserProfileById); 
+router.put('/user_profiles/:id', auth, upload.single('avatar_file'), userController.updateUserProfile); 
+router.get('/public_profile/:id', userController.getPublicUserProfile);
 router.get('/test-connection', userController.testConnection);
 router.post('/refresh-token', userController.refreshToken);
 router.get('/verify-token', userController.verifyToken);
-router.get('/basic_profile/:id', userController.getBasicUserProfile); // 新的公开路由
-router.put('/users/:id', auth, userController.updateUser); // 新添加的更新用户信息的路由
-router.get('/searcher_ids', userController.getSearcherIds); // 新添加的获取searcher用户ID的路由
+router.get('/basic_profile/:id', userController.getBasicUserProfile); 
+router.put('/users/:id', auth, userController.updateUser); 
+router.get('/searchers', userController.getSearchers); 
+router.get('/searcher_ids', userController.getSearcherIds); 
 
 module.exports = router;
