4a5e519f08d882eaa36bc787d63918f1d99a1ce0
MabsonThorne <mabsonthornne@gmail.com>
2024-07-02 14:06:32 +0800

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -19,6 +19,6 @@ router.post('/refresh-token', userController.refreshToken);
 router.get('/verify-token', userController.verifyToken);
 router.get('/basic_profile/:id', userController.getBasicUserProfile); // 新的公开路由
 router.put('/users/:id', auth, userController.updateUser); // 新添加的更新用户信息的路由
-router.get('/searchers', userController.getSearchers); // 新添加的获取searcher用户的路由
+router.get('/searcher_ids', userController.getSearcherIds); // 新添加的获取searcher用户ID的路由
 
 module.exports = router;
